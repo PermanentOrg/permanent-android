b71f1e45b70eba7be86b401df81444137febd770
Valase Ionut <ionut.valase@vspartners.us>
2020-09-02 10:51:25 +0300

    Changed hamburger icon and added menu item icons

--- app/src/main/java/org/permanent/permanent/ui/mainScreen/MainFragment.kt
+++ app/src/main/java/org/permanent/permanent/ui/mainScreen/MainFragment.kt
@@ -1,4 +1,4 @@
-package org.permanent.permanent.ui
+package org.permanent.permanent.ui.mainScreen
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,18 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.permanent.databinding.FragmentMainBinding
-import org.permanent.databinding.FragmentSecondaryBinding
 
-class SecondaryFragment:Fragment() {
+class MainFragment:Fragment() {
 
-    private lateinit var binding: FragmentSecondaryBinding
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = FragmentSecondaryBinding.inflate(inflater,container,false)
+        binding = FragmentMainBinding.inflate(inflater,container,false)
         binding.executePendingBindings()
         binding.lifecycleOwner=this
         return binding.root
--- app/src/main/java/org/permanent/permanent/ui/mainScreen/SecondaryFragment.kt
+++ app/src/main/java/org/permanent/permanent/ui/mainScreen/SecondaryFragment.kt
@@ -1,4 +1,4 @@
-package org.permanent.permanent.ui
+package org.permanent.permanent.ui.mainScreen
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,17 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.permanent.databinding.FragmentMainBinding
+import org.permanent.databinding.FragmentSecondaryBinding
 
-class MainFragment:Fragment() {
+class SecondaryFragment:Fragment() {
 
-    private lateinit var binding: FragmentMainBinding
+    private lateinit var binding: FragmentSecondaryBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = FragmentMainBinding.inflate(inflater,container,false)
+        binding = FragmentSecondaryBinding.inflate(inflater,container,false)
         binding.executePendingBindings()
         binding.lifecycleOwner=this
         return binding.root
