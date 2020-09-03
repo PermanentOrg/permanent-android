7fa52d7ca7c47f4c6199506e6a76abfdf3155585
FlauraHa <flaviahandrea@gmail.com>
2020-09-03 11:48:15 +0300

    Created a constants class.

---
+++ app/src/main/java/org/permanent/permanent/Constants.kt
@@ -0,0 +1,8 @@
+package org.permanent.permanent
+
+class Constants {
+    companion object {
+        const val IS_ONBOARDING_COMPLETED = "onboarding_completed"
+        const val IS_WELCOME_SEEN = "is_welcome_seen"
+    }
+}
