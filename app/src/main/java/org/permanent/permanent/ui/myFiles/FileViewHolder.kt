b9d42d270b059afb615a9fe601830b3d801acb22
FlauraHa <flaviahandrea@gmail.com>
2020-09-09 11:58:57 +0300

    Implemented my files search.

--- app/src/main/java/org/permanent/permanent/ui/myFiles/FileViewHolder.kt
+++ app/src/main/java/org/permanent/permanent/ui/myFiles/FileViewHolder.kt
@@ -1,10 +1,10 @@
 package org.permanent.permanent.ui.myFiles
 
 import androidx.recyclerview.widget.RecyclerView
-import org.permanent.databinding.FileBinding
+import org.permanent.databinding.ItemFileBinding
 import org.permanent.permanent.models.File
 
-class FileViewHolder(private val binding: FileBinding) : RecyclerView.ViewHolder(binding.root) {
+class FileViewHolder(private val binding: ItemFileBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(file: File) {
         binding.file = file
