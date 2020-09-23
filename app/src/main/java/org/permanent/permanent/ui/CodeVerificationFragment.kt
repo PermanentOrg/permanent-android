35617736907d0dcbdb3d2a340caf2e9d5ca840ce
Flavia <flavia.handrea@vspartners.us>
2020-09-23 16:18:56 +0300

    Implemented to open the main screen after calling the code verification successfully.

--- app/src/main/java/org/permanent/permanent/ui/CodeVerificationFragment.kt
+++ app/src/main/java/org/permanent/permanent/ui/CodeVerificationFragment.kt
@@ -1,18 +1,22 @@
 package org.permanent.permanent.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import org.permanent.permanent.databinding.FragmentVerificationCodeBinding
+import org.permanent.permanent.ui.mainScreen.MainActivity
 import org.permanent.permanent.viewmodels.CodeVerificationViewModel
 
-class CodeVerificationFragment : Fragment() {
+class CodeVerificationFragment : PermanentBaseFragment() {
 
     private lateinit var binding: FragmentVerificationCodeBinding
-    private lateinit var verificationViewModel: CodeVerificationViewModel
+    private lateinit var viewModel: CodeVerificationViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +24,41 @@ class CodeVerificationFragment : Fragment() {
         binding = FragmentVerificationCodeBinding.inflate(inflater, container, false)
         binding.executePendingBindings()
         binding.lifecycleOwner = this
-        verificationViewModel = ViewModelProvider(this).get(CodeVerificationViewModel::class.java)
-        binding.viewModel = verificationViewModel
+        viewModel = ViewModelProvider(this).get(CodeVerificationViewModel::class.java)
+        binding.viewModel = viewModel
         return binding.root
     }
 
+    private val onCodeVerified = Observer<Void> {
+        navigateCodeVerified()
+    }
+
+    private val onErrorMessage = Observer<String> { errorMessage ->
+        Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    private fun navigateCodeVerified() {
+        startActivity(Intent(context, MainActivity::class.java))
+        activity?.finish()
+    }
+
+    override fun connectViewModelEvents() {
+        viewModel.getOnCodeVerified().observe(this, onCodeVerified)
+        viewModel.getErrorMessage().observe(this, onErrorMessage)
+    }
+
+    override fun disconnectViewModelEvents() {
+        viewModel.getOnCodeVerified().removeObserver(onCodeVerified)
+        viewModel.getErrorMessage().removeObserver(onErrorMessage)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        connectViewModelEvents()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disconnectViewModelEvents()
+    }
 }
