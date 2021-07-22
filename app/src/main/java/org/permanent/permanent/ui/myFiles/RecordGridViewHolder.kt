cc3a0297d641573f0520f0106103e85724e5ef3b
Flavia Handrea <flavia.handrea@vspartners.us>
2021-07-22 10:49:17 +0300

    Not blurring files on SharePreview when access was granted.

--- app/src/main/java/org/permanent/permanent/ui/myFiles/RecordGridViewHolder.kt
+++ app/src/main/java/org/permanent/permanent/ui/myFiles/RecordGridViewHolder.kt
@@ -2,23 +2,32 @@ package org.permanent.permanent.ui.myFiles
 
 import android.content.Context
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.BlurTransformation
 import org.permanent.permanent.R
 import org.permanent.permanent.databinding.ItemGridRecordBinding
 import org.permanent.permanent.models.Record
+import org.permanent.permanent.ui.shares.PreviewState
 
 class RecordGridViewHolder(
     val context: Context, val binding: ItemGridRecordBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(record: Record, lifecycleOwner: LifecycleOwner) {
+    fun bind(
+        record: Record,
+        lifecycleOwner: LifecycleOwner,
+        previewState: MutableLiveData<PreviewState>
+    ) {
         binding.record = record
         binding.executePendingBindings()
         binding.lifecycleOwner = lifecycleOwner
-        if (record.isThumbBlurred != null && record.isThumbBlurred!!) {
 
+        if (record.isThumbBlurred != null
+            && record.isThumbBlurred!!
+            && previewState.value != PreviewState.ACCESS_GRANTED
+        ) {
             Picasso.get()
                 .load(record.thumbURL500)
                 .placeholder(R.drawable.ic_stop_light_grey)
