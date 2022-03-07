9b3fd570a9bbea215a36e69da5a8ad89825a61a9
Vlad Rusu <v@vladr.com>
2022-03-07 16:53:33 +0200

    VSP 490 [Android] File creation date does not match between webapp and mobile app

--- app/src/main/java/org/permanent/permanent/viewmodels/FileDetailsViewModel.kt
+++ app/src/main/java/org/permanent/permanent/viewmodels/FileDetailsViewModel.kt
@@ -1,10 +1,13 @@
 package org.permanent.permanent.viewmodels
 
+import android.annotation.SuppressLint
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import org.permanent.permanent.network.models.FileData
 import org.permanent.permanent.ui.bytesToHumanReadableString
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FileDetailsViewModel(application: Application) : ObservableAndroidViewModel(application) {
 
@@ -19,17 +22,36 @@ class FileDetailsViewModel(application: Application) : ObservableAndroidViewMode
     private val width = MutableLiveData("-")
     private val height = MutableLiveData("-")
 
+    @SuppressLint("SimpleDateFormat")
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
     fun setFileData(fileData: FileData) {
-        uploaded.value = fileData.createdDate ?: "-"
-        lastModified.value = fileData.updatedDate ?: "-"
-        created.value = fileData.derivedDate ?: "-"
-        fileCreated.value = fileData.derivedCreatedDate ?: "-"
+        uploaded.value = formattedDate(fileData.createdDate) ?: "-"
+        lastModified.value = formattedDate(fileData.updatedDate) ?: "-"
+        created.value = formattedDate(fileData.derivedDate) ?: "-"
+        fileCreated.value = formattedDate(fileData.derivedCreatedDate) ?: "-"
         size.value = if (fileData.size != -1L) bytesToHumanReadableString(fileData.size) else "-"
         fileType.value = fileData.contentType?.substringBefore("/") ?: "-"
         originalFileName.value = fileData.originalFileName ?: "-"
         originalFileType.value = fileData.originalFileType ?: "-"
         width.value = if (fileData.width != -1) fileData.width.toString() else "-"
         height.value = if (fileData.height != -1) fileData.height.toString() else "-"
+    }
+
+    fun formattedDate(date: String?): String? {
+        if (date != null) {
+            dateFormat.applyPattern("yyyy-MM-dd HH:mm:ss")
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+
+            val parsedDate = dateFormat.parse(date)
+
+            dateFormat.applyPattern("yyyy-MM-dd h:mm a zzz")
+            dateFormat.setTimeZone(TimeZone.getDefault())
+
+            return dateFormat.format(parsedDate)
+        } else {
+            return null
+        }
     }
 
     fun getUploaded(): LiveData<String> {
