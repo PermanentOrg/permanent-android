f3c02479905258ed82d3286e98a8d1702944a6c6
Flavia Handrea <flavia.handrea@vspartners.us>
2022-06-08 14:53:44 +0300

    Removed header for donation request. Updated versionCode.

--- app/src/main/java/org/permanent/permanent/network/IStorageService.kt
+++ app/src/main/java/org/permanent/permanent/network/IStorageService.kt
@@ -1,21 +1,22 @@
 package org.permanent.permanent.network
 
-import org.permanent.permanent.BuildConfig
 import org.permanent.permanent.network.models.ResponseVO
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import retrofit2.http.Url
 
 interface IStorageService {
 
-    @Headers("Authorization: Basic ${BuildConfig.SECRET_KEY_ENCODED}")
     @FormUrlEncoded
     @POST
     fun getPaymentIntent(
         @Url url: String,
-        @Field("accountId") accountId :Int,
+        @Field("accountId") accountId: Int,
         @Field("email") email: String?,
         @Field("name") name: String?,
         @Field("anonymous") isAnonymous: Boolean?,
-        @Field("amount") amount :Int,
+        @Field("amount") amount: Int,
     ): Call<ResponseVO>
 }
