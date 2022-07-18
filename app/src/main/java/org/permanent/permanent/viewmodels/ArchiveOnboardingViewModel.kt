060f92a3105f89954ede0feb1b52af8e567f10d3
Flavia Handrea <flavia.handrea@vspartners.us>
2022-07-18 17:23:02 +0300

    Set newly created archive as default and as current after archive onboarding.

--- app/src/main/java/org/permanent/permanent/viewmodels/ArchiveOnboardingViewModel.kt
+++ app/src/main/java/org/permanent/permanent/viewmodels/ArchiveOnboardingViewModel.kt
@@ -1,15 +1,24 @@
 package org.permanent.permanent.viewmodels
 
 import android.app.Application
+import android.content.Context
 import android.text.Editable
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import org.permanent.permanent.R
+import org.permanent.permanent.models.Account
+import org.permanent.permanent.models.Archive
 import org.permanent.permanent.models.ArchiveType
+import org.permanent.permanent.network.IDataListener
 import org.permanent.permanent.network.IResponseListener
+import org.permanent.permanent.network.models.Datum
+import org.permanent.permanent.repositories.AccountRepositoryImpl
 import org.permanent.permanent.repositories.ArchiveRepositoryImpl
+import org.permanent.permanent.repositories.IAccountRepository
 import org.permanent.permanent.repositories.IArchiveRepository
+import org.permanent.permanent.ui.PREFS_NAME
+import org.permanent.permanent.ui.PreferencesHelper
 import org.permanent.permanent.ui.archiveOnboarding.NameSettingFragment
 import org.permanent.permanent.ui.archiveOnboarding.OnboardingPage
 import org.permanent.permanent.ui.archiveOnboarding.StartFragment
@@ -19,6 +28,9 @@ class ArchiveOnboardingViewModel(application: Application) :
     ObservableAndroidViewModel(application) {
 
     private val appContext = application.applicationContext
+    private val prefsHelper = PreferencesHelper(
+        application.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    )
     private val isBusy = MutableLiveData(false)
     private val showMessage = SingleLiveEvent<String>()
     private val showError = SingleLiveEvent<String>()
@@ -35,6 +47,7 @@ class ArchiveOnboardingViewModel(application: Application) :
     private var typeSelectionFragment = TypeSelectionFragment()
     private var nameSettingFragment = NameSettingFragment()
     private var archiveRepository: IArchiveRepository = ArchiveRepositoryImpl(application)
+    private var accountRepository: IAccountRepository = AccountRepositoryImpl(application)
 
     fun onArchiveTypeBtnClick(archiveType: ArchiveType) {
         isArchiveSelected.value = true
@@ -82,7 +95,7 @@ class ArchiveOnboardingViewModel(application: Application) :
         if (isBusy.value != null && isBusy.value!!) {
             return
         }
-        val name = name.value
+        val name = name.value?.trim()
         val selectedArchiveType = selectedArchiveType.value
 
         if (name != null && selectedArchiveType != null) {
@@ -90,11 +103,10 @@ class ArchiveOnboardingViewModel(application: Application) :
             archiveRepository.createNewArchive(
                 name,
                 selectedArchiveType,
-                object : IResponseListener {
-                    override fun onSuccess(message: String?) {
+                object : IArchiveRepository.IArchiveListener {
+                    override fun onSuccess(archive: Archive) {
                         isBusy.value = false
-                        onArchiveCreated.call()
-                        message?.let { showMessage.value = it }
+                        setNewArchiveAsDefault(archive)
                     }
 
                     override fun onFailed(error: String?) {
@@ -102,6 +114,48 @@ class ArchiveOnboardingViewModel(application: Application) :
                         error?.let { showError.value = it }
                     }
                 })
+        }
+    }
+
+    fun setNewArchiveAsDefault(newArchive: Archive) {
+        val account = Account(prefsHelper.getAccountId(), prefsHelper.getAccountEmail())
+        account.defaultArchiveId = newArchive.id
+
+        accountRepository.update(account, object : IResponseListener {
+            override fun onSuccess(message: String?) {
+                isBusy.value = false
+                prefsHelper.saveDefaultArchiveId(newArchive.id)
+                setNewArchiveAsCurrent(newArchive)
+            }
+
+            override fun onFailed(error: String?) {
+                isBusy.value = false
+                error?.let { showError.value = it }
+            }
+        })
+    }
+
+    fun setNewArchiveAsCurrent(newArchive: Archive) {
+        newArchive.number?.let { archiveNr ->
+            archiveRepository.switchToArchive(archiveNr, object : IDataListener {
+                override fun onSuccess(dataList: List<Datum>?) {
+                    isBusy.value = false
+                    prefsHelper.saveCurrentArchiveInfo(
+                        newArchive.id,
+                        newArchive.number,
+                        newArchive.type,
+                        newArchive.fullName,
+                        newArchive.thumbURL200,
+                        newArchive.accessRole
+                    )
+                    onArchiveCreated.call()
+                }
+
+                override fun onFailed(error: String?) {
+                    isBusy.value = false
+                    error?.let { showError.value = it }
+                }
+            })
         }
     }
 
