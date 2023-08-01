b06fd430f45fd85cf91f15139f78a99346d5bbf7
Flavia Handrea <flavia.handrea@vspartners.us>
2023-08-01 10:57:48 +0300

    Implemented Designate Contact screen. Did some refactoring.

--- app/src/main/java/org/permanent/permanent/ui/legacyPlanning/DesignateContactOrStewardFragment.kt
+++ app/src/main/java/org/permanent/permanent/ui/legacyPlanning/DesignateContactOrStewardFragment.kt
@@ -6,29 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import org.permanent.permanent.R
 import org.permanent.permanent.ui.PermanentBaseFragment
-import org.permanent.permanent.ui.compose.IntroScreen
-import org.permanent.permanent.viewmodels.IntroViewModel
+import org.permanent.permanent.ui.compose.DesignateContactOrStewardScreen
 
-class IntroFragment : PermanentBaseFragment() {
-    private lateinit var viewModel: IntroViewModel
+class DesignateContactOrStewardFragment : PermanentBaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        viewModel = ViewModelProvider(this)[IntroViewModel::class.java]
-
         return ComposeView(requireContext()).apply {
             setContent {
                 MaterialTheme {
-                    IntroScreen(navigateToDesignateContactScreen = {
-                        findNavController().navigate(R.id.action_introFragment_to_designateContactOrStewardFragment)
-                    }, onCloseScreen = {
-                        findNavController().popBackStack(R.id.myFilesFragment, false)
-                    })
+                    DesignateContactOrStewardScreen()
                 }
             }
         }
