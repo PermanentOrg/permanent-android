dd124ede061e9ddc0b5c0fdc2252e45d0285740f
flaviuvsp <flaviu.silaghi@vspartners.us>
2023-08-23 12:19:15 +0300

    Add Mixpanel to project

--- app/src/main/java/org/permanent/permanent/ui/activities/MainActivity.kt
+++ app/src/main/java/org/permanent/permanent/ui/activities/MainActivity.kt
@@ -32,6 +32,7 @@ import com.google.android.gms.common.GoogleApiAvailability
 import org.permanent.permanent.BuildConfig
 import org.permanent.permanent.Constants.Companion.REQUEST_CODE_GOOGLE_API_AVAILABILITY
 import org.permanent.permanent.CurrentArchivePermissionsManager
+import org.permanent.permanent.EventsManager
 import org.permanent.permanent.R
 import org.permanent.permanent.RECIPIENT_ARCHIVE_NAME_KEY
 import org.permanent.permanent.RECIPIENT_ARCHIVE_NR_KEY
@@ -215,7 +216,10 @@ class MainActivity : PermanentBaseActivity(), Toolbar.OnMenuItemClickListener {
                     return@setNavigationItemSelectedListener false
                 }
 
-                R.id.logOut -> viewModel.deleteDeviceToken()
+                R.id.logOut -> {
+                    viewModel.deleteDeviceToken()
+                    EventsManager(this).resetUser()
+                }
                 else -> {
                     menuItem.onNavDestinationSelected(navController)
                     binding.drawerLayout.closeDrawers()
