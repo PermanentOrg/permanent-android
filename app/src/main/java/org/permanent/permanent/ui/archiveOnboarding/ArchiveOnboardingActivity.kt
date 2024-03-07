370d6d419d2e0e9763c17d19ff13298f438c8501
Flavia Handrea <flavia.handrea@vspartners.us>
2024-03-07 16:24:30 +0200

    Archive Onboarding Container.

--- app/src/main/java/org/permanent/permanent/ui/archiveOnboarding/ArchiveOnboardingActivity.kt
+++ app/src/main/java/org/permanent/permanent/ui/archiveOnboarding/ArchiveOnboardingActivity.kt
@@ -1,32 +1,19 @@
 package org.permanent.permanent.ui.archiveOnboarding
 
-import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.graphics.Typeface
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.snackbar.Snackbar
 import org.permanent.permanent.R
 import org.permanent.permanent.databinding.ActivityArchiveOnboardingBinding
 import org.permanent.permanent.ui.PREFS_NAME
 import org.permanent.permanent.ui.PreferencesHelper
-import org.permanent.permanent.ui.activities.MainActivity
 import org.permanent.permanent.ui.activities.PermanentBaseActivity
 import org.permanent.permanent.ui.computeWindowSizeClasses
-import org.permanent.permanent.viewmodels.ArchiveOnboardingViewModel
 
 class ArchiveOnboardingActivity : PermanentBaseActivity() {
 
     private lateinit var prefsHelper: PreferencesHelper
     private lateinit var binding: ActivityArchiveOnboardingBinding
-    private lateinit var viewModel: ArchiveOnboardingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,58 +30,52 @@ class ArchiveOnboardingActivity : PermanentBaseActivity() {
         prefsHelper.saveWindowWidthSizeClass(windowWidthSizeClass)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_archive_onboarding)
-        viewModel = ViewModelProvider(this)[ArchiveOnboardingViewModel::class.java]
         binding.executePendingBindings()
         binding.lifecycleOwner = this
-        binding.viewModel = viewModel
-
-        setSupportActionBar(binding.toolbar)
     }
 
-    private fun showFragment(fragment: Fragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frameLayoutContainer, fragment).commit()
-    }
-
-    private val onShowMessage = Observer<String> { message ->
-        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
-        val view: View = snackBar.view
-        view.setBackgroundColor(ContextCompat.getColor(this, R.color.deepGreen))
-        snackBar.setTextColor(ContextCompat.getColor(this, R.color.paleGreen))
-        val snackbarTextTextView = view.findViewById(R.id.snackbar_text) as TextView
-        snackbarTextTextView.setTypeface(snackbarTextTextView.typeface, Typeface.BOLD)
-        snackBar.show()
-    }
-
-    private val onShowError = Observer<String> { message ->
-        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
-        val view: View = snackBar.view
-        view.setBackgroundColor(ContextCompat.getColor(this, R.color.deepRed))
-        snackBar.setTextColor(ContextCompat.getColor(this, R.color.white))
-        snackBar.show()
-    }
-
-    private val onShowNextFragment = Observer<Fragment> {
-        showFragment(it)
-    }
-
-    private val onArchiveOnboardingDone = Observer<Void?> {
-        startActivity(Intent(this@ArchiveOnboardingActivity, MainActivity::class.java))
-        finish()
-    }
-
+    //
+//    private val onShowMessage = Observer<String> { message ->
+//        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+//        val view: View = snackBar.view
+//        view.setBackgroundColor(ContextCompat.getColor(this, R.color.deepGreen))
+//        snackBar.setTextColor(ContextCompat.getColor(this, R.color.paleGreen))
+//        val snackbarTextTextView = view.findViewById(R.id.snackbar_text) as TextView
+//        snackbarTextTextView.setTypeface(snackbarTextTextView.typeface, Typeface.BOLD)
+//        snackBar.show()
+//    }
+//
+//    private val onShowError = Observer<String> { message ->
+//        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+//        val view: View = snackBar.view
+//        view.setBackgroundColor(ContextCompat.getColor(this, R.color.deepRed))
+//        snackBar.setTextColor(ContextCompat.getColor(this, R.color.white))
+//        snackBar.show()
+//    }
+//
+//    private val onShowNextFragment = Observer<Fragment> {
+//        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+//        transaction.replace(R.id.frameLayoutContainer, it).commit()
+//    }
+//
+//    private val onArchiveOnboardingDone = Observer<Void?> {
+//        startActivity(Intent(this@ArchiveOnboardingActivity, MainActivity::class.java))
+//        finish()
+//    }
+//
     override fun connectViewModelEvents() {
-        viewModel.getShowMessage().observe(this, onShowMessage)
-        viewModel.getShowError().observe(this, onShowError)
-        viewModel.getOnShowNextFragment().observe(this, onShowNextFragment)
-        viewModel.getOnArchiveOnboardingDone().observe(this, onArchiveOnboardingDone)
+//        viewModel.getShowMessage().observe(this, onShowMessage)
+//        viewModel.getShowError().observe(this, onShowError)
+//        viewModel.getOnShowNextFragment().observe(this, onShowNextFragment)
+//        viewModel.getOnArchiveOnboardingDone().observe(this, onArchiveOnboardingDone)
     }
 
+    //
     override fun disconnectViewModelEvents() {
-        viewModel.getShowMessage().removeObserver(onShowMessage)
-        viewModel.getShowError().removeObserver(onShowError)
-        viewModel.getOnShowNextFragment().removeObserver(onShowNextFragment)
-        viewModel.getOnArchiveOnboardingDone().removeObserver(onArchiveOnboardingDone)
+//        viewModel.getShowMessage().removeObserver(onShowMessage)
+//        viewModel.getShowError().removeObserver(onShowError)
+//        viewModel.getOnShowNextFragment().removeObserver(onShowNextFragment)
+//        viewModel.getOnArchiveOnboardingDone().removeObserver(onArchiveOnboardingDone)
     }
 
     override fun onResume() {
