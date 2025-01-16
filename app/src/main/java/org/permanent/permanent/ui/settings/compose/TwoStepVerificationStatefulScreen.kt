1616f6b844aa49154ddf583096ff354eaef6c7e6
Flavia Handrea <flavia.handrea@vspartners.us>
2025-01-16 12:46:47 +0200

    2FA Confirm password screen

--- app/src/main/java/org/permanent/permanent/ui/settings/compose/TwoStepVerificationStatefulScreen.kt
+++ app/src/main/java/org/permanent/permanent/ui/settings/compose/TwoStepVerificationStatefulScreen.kt
@@ -1,8 +1,17 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.permanent.permanent.ui.settings.compose
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.launch
 import org.permanent.permanent.viewmodels.TwoStepVerificationViewModel
 
 @Composable
@@ -10,19 +19,58 @@ fun TwoStepVerificationStatefulScreen(
     viewModel: TwoStepVerificationViewModel
 ) {
     val isTwoFAEnabled by viewModel.isTwoFAEnabled.collectAsState()
+    val scope = rememberCoroutineScope()
+
+    // State to control the visibility of the bottom sheet dialog
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true // Ensures it opens fully
+    )
+    var onPasswordConfirmed by remember { mutableStateOf({}) }
+    var showBottomSheet by remember { mutableStateOf(false) } // Controls visibility
+
+    if (showBottomSheet) {
+        PasswordConfirmationBottomSheet(
+            viewModel,
+            sheetState = bottomSheetState,
+            onDismiss = {
+                scope.launch {
+                    bottomSheetState.hide() // Hides the sheet with animation
+                    showBottomSheet = false // Ensure it can be reopened later
+                }
+            },
+            onConfirm = { password ->
+                viewModel.verifyPassword(password) { errorMessage ->
+                    if (errorMessage == null) {
+                        onPasswordConfirmed()
+                    }
+                }
+            }
+        )
+    }
 
     if (isTwoFAEnabled) {
-        // Show the enabled version of the screen
-        TwoStepVerificationEnabledScreen(
-            viewModel,
-            onChangeVerificationMethodClick = {
+        TwoStepVerificationEnabledScreen(viewModel, onChangeVerificationMethodClick = {
+            onPasswordConfirmed = {
+                // Navigate to change verification method screen
             }
-        )
+            scope.launch {
+                if (!bottomSheetState.isVisible) {
+                    showBottomSheet = true
+                    bottomSheetState.show() // Animates the sheet to visible
+                }
+            }
+        })
     } else {
-        // Show the disabled version of the screen
-        TwoStepVerificationDisabledScreen(
-            onAddTwoStepVerificationClick = {
+        TwoStepVerificationDisabledScreen(onAddTwoStepVerificationClick = {
+            onPasswordConfirmed = {
+                // Navigate to enable 2FA screen
             }
-        )
+            scope.launch {
+                if (!bottomSheetState.isVisible) {
+                    showBottomSheet = true
+                    bottomSheetState.show() // Animates the sheet to visible
+                }
+            }
+        })
     }
 }
